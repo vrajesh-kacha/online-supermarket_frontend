f70132d8abbd8471285a0cfa7b450e276c4707a5
vrajesh-kacha <kachavrajesh94@gmail.com>
2024-10-26 13:59:14 +0530

    configure vite for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-    include: "src/**/*.jsx",
-  })]
+  plugins: [react()],
+  base: "/"
 })
